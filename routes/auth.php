06a5ea0f09b4c7bdd9a33cfcdab44255b59cba0b
siwakasen <siwakasen@gmail.com>
2024-12-20 16:20:42 +0700

    change name redire login

--- routes/auth.php
+++ routes/auth.php
@@ -18,7 +18,7 @@ Route::middleware('guest')->group(function () {
     Route::post('register', [RegisteredApplicantController::class, 'store'])->name('register');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+        ->name('login-redirect');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
 
